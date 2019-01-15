475e250bcd9b4128e0c901e4a2e7da352c870605
Christina <christina.hsu.ca@gmail.com>
2019-01-15 02:01:59 +0000

    photos added

--- download_avatars.js
+++ download_avatars.js
@@ -2,42 +2,47 @@
  var secret = require('./secret');
  var fs = require('fs');
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//     //make request inside the function
-//     var options = {
-//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//         headers: {
-//             'User-Agent': 'request',
-//             'Authorization': 'token ' + secret.GITHUB_TOKEN
-//         }
-//     };
+function getRepoContributors(repoOwner, repoName, cb) {
+    //make request inside the function
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + secret.GITHUB_TOKEN
+        }
+    };
         
-//         request(options, function(err, res, body) {
-//             var data = JSON.parse(body);
-//             cb(err, data);
+        request(options, function(err, res, body) {
+            var data = JSON.parse(body);
+            cb(err, data);
             
-//     });
+    });
 
-// }
+}
 
-// getRepoContributors('jquery', 'jquery', function(err, result) {
-//     console.log("Errors:", err);
-//     result.forEach(function(contributor) {
-//         console.log("Result:", contributor['avatar_url']);
-//     });
-// })
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+    console.log("Errors:", err);
+    var fileName = '';
+    var url = '';
+    result.forEach(function(contributor) {
+        url = contributor.avatar_url;
+        fileName = './uploads/' + contributor.login + '.jpg';
+        downloadImageByURL(url, fileName);
+    });
+})
 
 function downloadImageByURL(url, filePath) {
-    request.get(url)
+    console.log(filePath);
+    request(url)
         .on('error', function(err) {
             throw err;
         })
-        .pipe(fs.createWriteStream(filePath));
-    
-       //console.log(body); 
-    
+        .pipe(fs.createWriteStream(filePath))
+        .on('finish', function() {
+            console.log('download finished');
+        })
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
