49e0e00d253d74a7925a18a30737d35a16e10652
Christina <christina.hsu.ca@gmail.com>
2019-01-15 01:38:36 +0000

    photo download functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,43 @@
-var request = require('request');
-var secret = require('./secret');
+ var request = require('request');
+ var secret = require('./secret');
+ var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-    //make request inside the function
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        headers: {
-            'User-Agent': 'request',
-            'Authorization': 'token ' + secret.GITHUB_TOKEN
-        }
-    };
+// function getRepoContributors(repoOwner, repoName, cb) {
+//     //make request inside the function
+//     var options = {
+//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//         headers: {
+//             'User-Agent': 'request',
+//             'Authorization': 'token ' + secret.GITHUB_TOKEN
+//         }
+//     };
         
-        request(options, function(err, res, body) {
-            var data = JSON.parse(body);
-            cb(err, data);
+//         request(options, function(err, res, body) {
+//             var data = JSON.parse(body);
+//             cb(err, data);
             
-    });
+//     });
 
+// }
+
+// getRepoContributors('jquery', 'jquery', function(err, result) {
+//     console.log("Errors:", err);
+//     result.forEach(function(contributor) {
+//         console.log("Result:", contributor['avatar_url']);
+//     });
+// })
+
+function downloadImageByURL(url, filePath) {
+    request.get(url)
+        .on('error', function(err) {
+            throw err;
+        })
+        .pipe(fs.createWriteStream(filePath));
+    
+       //console.log(body); 
+    
 }
 
-getRepoContributors('jquery', 'jquery', function(err, result) {
-    console.log("Errors:", err);
-    result.forEach(function(contributor) {
-        console.log("Result:", contributor['avatar_url']);
-    });
-})
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
