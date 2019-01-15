a97f5d0b5b2c86c8a9e924fda7051a011daac690
Christina <christina.hsu.ca@gmail.com>
2019-01-15 01:08:00 +0000

    update the url result

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,29 @@
 var request = require('request');
+var secret = require('./secret');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+    //make request inside the function
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + secret.GITHUB_TOKEN
+        }
+    };
+        
+        request(options, function(err, res, body) {
+            var data = JSON.parse(body);
+            cb(err, data);
+            
+    });
 
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    result.forEach(function(contributor) {
+        console.log("Result:", contributor['avatar_url']);
+    });
 })
